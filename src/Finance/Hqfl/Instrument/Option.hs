0bc0eb60cf7bd7059f4b5e177d8249903cfee6b5
Michael Khan <co.kleisli@gmail.com>
2016-06-02 00:29:23 +0100

    remove unnecessary data types and factor out common code

--- src/Finance/Hqfl/Instrument/Option.hs
+++ src/Finance/Hqfl/Instrument/Option.hs
@@ -4,17 +4,14 @@
 -- Copyright   :  (C) 2016 Mika'il Khan
 -- License     :  (see the file LICENSE)
 -- Maintainer  :  Mika'il Khan <co.kleisli@gmail.com>
--- Stability   :  experimental
--- Portability :  non-portable
+-- Stability   :  stable
+-- Portability :  portable
 --
 ----------------------------------------------------------------------------   
 
 module Finance.Hqfl.Instrument.Option
 (
-  Option(..),
-  Type(..),
-  Style(..),
-  Exotic(..)
+  Option(..)
 )
 where
 
@@ -59,9 +56,6 @@ Exotic options are non-Vanilla options:
 -- TODO: Function to get the current price of the underlying
 -- TODO: Typeclasses to categorise options according to their pricing function: American, European - each instance to supply pricing method
 
-data Type = Call | Put deriving (Eq, Show)
-data Style = European | American deriving (Eq, Show)-- Vanilla
-data Exotic = Bermudan | Asian | Barrier | Binary deriving (Eq, Show) -- Exotics
 
 data Option a = Option a Type Style Strike Maturity deriving (Show)
 
