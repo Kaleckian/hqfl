aefec7e5cf7e02fbac29fe072f0aca7de476adc9
Michael Khan <co.kleisli@gmail.com>
2016-06-02 00:31:27 +0100

    add new black-scholes pricer

---
+++ src/Finance/Hqfl/Pricer/BlackScholes.hs
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Finance.Hqfl.Instrument.BlackScholes
+-- Copyright   :  (C) 2016 Mika'il Khan
+-- License     :  (see the file LICENSE)
+-- Maintainer  :  Mika'il Khan <co.kleisli@gmail.com>
+-- Stability   :  stable
+-- Portability :  portable
+--
+----------------------------------------------------------------------------   
+
+module Finance.Hqfl.Pricer.BlackScholes where
