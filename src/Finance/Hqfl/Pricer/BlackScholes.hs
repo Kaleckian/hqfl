72872579a993f0068c78f125c861a3039c24b612
Michael Khan <co.kleisli@gmail.com>
2016-06-16 12:53:39 +0100

    add pricer for european barrier options

--- src/Finance/Hqfl/Pricer/BlackScholes.hs
+++ src/Finance/Hqfl/Pricer/BlackScholes.hs
@@ -23,9 +23,9 @@ call s k r v t q = s * exp(-q * t) * (cdf normal d1)  - k * exp(-r * t) * (cdf n
         d2 = d1 - v * sqrt t
         normal = Normal (0 :: Double) 1
 
-put :: Double -> Double -> Double -> Double -> Double -> Double
-put s k r v t = k * exp(-r * t) * (cdf normal (-d2)) - s * (cdf normal (-d1))
-  where d1 = (log (s / k) + (r + (v * v) / 2) * t) / (v * sqrt t)
+put :: Double -> Double -> Double -> Double -> Double -> Double -> Double
+put s k r v t q = k * exp(-r * t) * (cdf normal (-d2)) - s * exp(-q * t) * (cdf normal (-d1))
+  where d1 = (log (s / k) + (r - q + (v * v) / 2) * t) / (v * sqrt t)
         d2 = d1 - v * sqrt t
         normal = Normal (0 :: Double) 1
 
@@ -37,24 +37,97 @@ cdi s k r v t q h = s * exp(-q * t) * (h / s)**(2 * lambda) * (cdf normal y)
         normal = Normal (0 :: Double) 1
 
 cdo :: Double -> Double -> Double -> Double -> Double -> Double -> Double -> Double
-cdo s k r v t q h = call s k r v t q - cdi s k r v t q h
+cdo s k r v t q h = s * (cdf normal x1) * exp (-q * t) - k * exp(-r * t) * (cdf normal (x1 - v * sqrt t)) -
+                    s * exp(-q * t) * (h / s)**(2 * lambda) * (cdf normal y1) + k * exp(-r * t) * (h/ s)**(2 * lambda - 2) * (cdf normal (y1 - v * sqrt t))
+  where lambda = ((r - q + (v * v) / 2)) / (v * v)
+        y1 = (log (h / s) / v * sqrt t) + (lambda * v * sqrt t)
+        x1 = (log ( s / h ) / (v * sqrt t)) + (lambda * v * sqrt t)
+        normal = Normal (0 :: Double) 1
 
+cui :: Double -> Double -> Double -> Double -> Double -> Double -> Double -> Double
+cui s k r v t q h = s * (cdf normal x1) * exp(-q * t) - k * exp(-r * t) * (cdf normal (x1 - v * sqrt t)) -
+      s * exp(-q *t) * (h / s)**(2 * lambda) * ((cdf normal (-y)) - (cdf normal (-y1))) +
+      k * exp(-r * t) * (h / s)**(2 * lambda - 2) * ((cdf normal (-y + v * sqrt t)) - (cdf normal (-y1 + v * sqrt t)))
+  where lambda = ((r - q + (v * v) / 2)) / (v * v)
+        y1 = (log (h / s) / v * sqrt t) + (lambda * v * sqrt t)
+        x1 = (log ( s / h ) / (v * sqrt t)) + (lambda * v * sqrt t)
+        y = (log ( (h*h) / (s * k)) / v * sqrt t) + (lambda * v * sqrt t)
+        normal = Normal (0 :: Double) 1
 
+pui :: Double -> Double -> Double -> Double -> Double -> Double -> Double -> Double
+pui s k r v t q h = -s * exp(-q * t) * (h / s)**(2 * lambda) * (cdf normal (-y)) + k * exp(-r * t) * (h / s)**(2 * lambda - 2) * (cdf normal ((-y) + (v * sqrt t)))
+  where lambda = ((r - q + (v * v) / 2)) / (v * v)
+        y = (log ( (h*h) / (s * k)) / v * sqrt t) + (lambda * v * sqrt t)
+        normal = Normal (0 :: Double) 1
+
+pdi :: Double -> Double -> Double -> Double -> Double -> Double -> Double -> Double
+pdi s k r v t q h = -s * (cdf normal (-x1)) * exp(-q * t)
+                    + k * exp(-r * t) * (cdf normal (-x1 + v * sqrt t))
+                    + s * exp(-q * t) * (h / s)**(2 * lambda) * (cdf normal y - cdf normal y1)
+                    - k * exp(-r * t) * (h / s)**(2 * lambda -2) * (cdf normal (y - v * sqrt t) - cdf normal (y1 - v * sqrt t))
+  where lambda = ((r - q + (v * v) / 2)) / (v * v)
+        y1 = (log (h / s) / v * sqrt t) + (lambda * v * sqrt t)
+        x1 = (log ( s / h ) / (v * sqrt t)) + (lambda * v * sqrt t)
+        y = (log ( (h*h) / (s * k)) / v * sqrt t) + (lambda * v * sqrt t)
+        normal = Normal (0 :: Double) 1
+        
+puo s k r v t q h = -s * (cdf normal (-x1)) * exp(-q * t) + k * exp(-r * t) * (cdf normal (-x1 + v * sqrt t)) +
+                    s * exp(-q * t) * (h / s)**(2 * lambda) * (cdf normal (-y1)) - k * exp(-r * t) * (h / s)**(2 * lambda -2) * (cdf normal ((-y1) + v * sqrt t))
+  where lambda = ((r - q + (v * v) / 2)) / (v * v)
+        y1 = (log (h / s) / v * sqrt t) + (lambda * v * sqrt t)
+        x1 = (log ( s / h ) / (v * sqrt t)) + (lambda * v * sqrt t)
+        y = (log ( (h*h) / (s * k)) / v * sqrt t) + (lambda * v * sqrt t)
+        normal = Normal (0 :: Double) 1
+    
 class BS a where
   blackscholesprice :: a -> Double -> Double -> Either String Double
 
 instance BS (Option Equity) where
   blackscholesprice (Option (Equity s) Call European k t q) r v = Right $ call s k r v t q
-  blackscholesprice (Option (Equity s) Put European k t q) r v = Right $ put s k r v t
+  blackscholesprice (Option (Equity s) Put European k t q) r v = Right $ put s k r v t q
 
-{-
- What we are pricing here is the chance that given that the stock is at price s now and the barrier is h, given all the other parameters, the chance that the stock will hit the barrier or not is given by the BS equation.
--}
+-- TODO : factor in rebates
 instance BS (BarrierOption Equity) where
-  blackscholesprice (BarrierOption (Equity s) DownAndIn European Call h k t q) r v
-    | s <= h = Left $ "Contract is converted to a vanilla contract"
-    | h <= k = Right $ cdi s k r v t q h
-  blackscholesprice (BarrierOption (Equity s) DownAndOut European Call h k t q) r v
-    | s <= h = Left $ "Contract is void"
-    | h <= k = Right $ cdo s k r v t q h
-
+  blackscholesprice (BarrierOption (Equity s) x European y h k t q) r v =
+    case (x, y) of
+      (DownAndIn, Call)
+        | s <= h -> Left "Contract is converted to a vanilla call"
+        | h <= k -> Right cdi'
+        | h >= k -> Right $ call' - cdo'
+      (DownAndOut, Call)
+        | s <= h -> Left "Contract is void"
+        | h <= k -> Right $ call' - cdi'
+        | h >= k -> Right cdo'
+      (UpAndIn, Call)
+        | h <= s -> Left "Contract is converted to a vanilla call"
+        | h <= k -> Right call'
+        | h >= k -> Right $ cui'
+      (UpAndOut, Call)
+        | s <= h -> Left "Contract is void"
+        | h <= k -> Right $ call' - cdi'
+        | h >= k -> Right cdo'
+      (DownAndIn, Put)
+        | s <= h -> Left "Contract is converted to a vanilla call"
+        | h <  k -> Right pdi'
+        | h >= k -> Right put'
+      (DownAndOut, Put)
+        | s <= h -> Left "Contract is void"
+        | h <  k -> Right $ put' - pdi'
+        | h >=  k -> Right 0
+      (UpAndIn, Put)
+        | h <= s -> Left "Contract is converted to a vanilla call"
+        | h <= k -> Right $ put' - puo'
+        | h >= k -> Right pui'
+      (UpAndOut, Put)
+        | h <= s -> Left "Contract is void"
+        | h <= k -> Right $ puo'
+        | h >= k -> Right $ put' - pui'
+    where
+      call' = call s k r v t q 
+      cdi'  = cdi  s k r v t q h
+      cdo'  = cdo  s k r v t q h
+      cui'  = cui  s k r v t q h
+      pdi'  = pdi  s k r v t q h
+      put'  = put  s k r v t q
+      pui'  = pui  s k r v t q h
+      puo'  = puo  s k r v t q h
--- test/Finance/Hqfl/Pricer/BlackScholesSpec.hs
+++ test/Finance/Hqfl/Pricer/BlackScholesSpec.hs
@@ -19,3 +19,95 @@ spec = do
      
     it "returns the price of a vanilla put option" $ do
       blackscholesprice (Option (Equity 42) Put European 40 0.5 0) 0.1 0.2 `shouldBe` Right 0.8085993729000958
+
+    context "Given a Down-and-In European Call Equity Barrier Option" $ do
+      context "When the stock price is less than the barrier" $ do
+        it "the contract is converted to a vanilla call" $ do
+          blackscholesprice (BarrierOption (Equity 80) DownAndIn European Call 90 100 1.0 0.05) 0.1 0.15 `shouldBe` Left "Contract is converted to a vanilla call"
+
+      context "When the barrier price is less than the strike" $ do
+        it "the price is calculated" $ do
+          blackscholesprice (BarrierOption (Equity 100) DownAndIn European Call 90 100 1.0 0.05) 0.1 0.15 `shouldBe` Right 0.6312064114470974
+          
+      context "When the barrier price is greater than the strike" $ do
+        it "the price is calculated" $ do
+          blackscholesprice (BarrierOption (Equity 100) DownAndIn European Call 95 90 1.0 0.05) 0.1 0.15 `shouldBe` Right 6.275436676978629
+
+    context "Given a Down-and-Out European Call Equity Barrier Option" $ do
+      context "When the stock price is less than the barrier" $ do
+        it "the contract is voided" $ do
+          blackscholesprice (BarrierOption (Equity 80) DownAndOut European Call 90 100 1.0 0.05) 0.1 0.15 `shouldBe` Left "Contract is void"
+
+      context "When the barrier price is less than the strike" $ do
+        it "the price is calculated" $ do
+          blackscholesprice (BarrierOption (Equity 100) DownAndOut European Call 90 100 1.0 0.05) 0.1 0.15 `shouldBe` Right 7.541431780268267
+          
+      context "When the barrier price is greater than the strike" $ do
+        it "the price is calculated" $ do
+          blackscholesprice (BarrierOption (Equity 100) DownAndOut European Call 95 90 1.0 0.05) 0.1 0.15 `shouldBe` Right 8.437380137422835
+          
+    context "Given a Up-and-In European Call Equity Barrier Option" $ do
+      context "When the stock price is less than the barrier" $ do
+        it "the contract is converted to a vanilla call" $ do
+          blackscholesprice (BarrierOption (Equity 100) UpAndIn European Call 90 100 1 0.05) 0.10  0.15 `shouldBe` Left "Contract is converted to a vanilla call"
+
+      context "When the barrier price is less than the strike" $ do
+        it "the price is calculated" $ do
+          blackscholesprice (BarrierOption (Equity 60) UpAndIn European Call 90 100 1 0.05) 0.10  0.15 `shouldBe` Right 3.177304025489283e-3
+          
+      context "When the barrier price is greater than the strike" $ do
+        it "the price is calculated" $ do
+          blackscholesprice (BarrierOption (Equity 60) UpAndIn European Call 90 70 1 0.05) 0.10  0.15 `shouldBe` Right 0.2527658929608891
+                    
+    context "Given a Down-and-In European Put Equity Barrier Option" $ do
+      context "When the stock price is less than the barrier" $ do
+        it "the contract is converted to a vanilla call" $ do
+          blackscholesprice (BarrierOption (Equity 80) DownAndIn European Put 90 100 1 0.05) 0.10  0.15 `shouldBe` Left "Contract is converted to a vanilla call"
+
+      context "When the barrier price is less than the strike" $ do
+        it "the price is calculated" $ do
+          blackscholesprice (BarrierOption (Equity 100) DownAndIn European Put 90 100 1 0.05) 0.10  0.15 `shouldBe` Right 3.255754346302549
+          
+      context "When the barrier price is greater than the strike" $ do
+        it "the price is calculated" $ do
+          blackscholesprice (BarrierOption (Equity 100) DownAndIn European Put 90 80 1 0.05) 0.10  0.15 `shouldBe` Right 0.1681659314048356
+          
+    context "Given a Down-and-Out European Put Equity Barrier Option" $ do
+      context "When the stock price is less than the barrier" $ do
+        it "it is converted to a vanilla call" $ do
+          blackscholesprice (BarrierOption (Equity 80) DownAndOut European Put 90 100 1 0.05) 0.10  0.15 `shouldBe` Left "Contract is void"
+
+      context "When the barrier price is less than the strike" $ do
+        it "the barrier option is pricexbxbd" $ do
+          blackscholesprice (BarrierOption (Equity 100) DownAndOut European Put 90 100 1 0.05) 0.10  0.15 `shouldBe` Right 0.27768319893735693
+          
+      context "When the barrier price is greater than the strike" $ do
+        it "the price is calculated" $ do
+          blackscholesprice (BarrierOption (Equity 100) DownAndOut European Put 90 80 1 0.05) 0.10  0.15 `shouldBe` Right 0.0
+
+    context "Given a Up-and-In European Put Equity Barrier Option" $ do
+      context "When the stock price is less than the barrier" $ do
+        it "it is converted to a vanilla call" $ do
+          blackscholesprice (BarrierOption (Equity 100) UpAndIn European Put 90 100 1 0.05) 0.10 0.15 `shouldBe` Left "Contract is converted to a vanilla call"
+
+      context "When the barrier price is less than the strike" $ do
+        it "the price is calculated" $ do
+          blackscholesprice (BarrierOption (Equity 80) UpAndIn European Put 90 100 1 0.05) 0.10 0.15 `shouldBe` Right 4.289037317247146
+          
+      context "When the barrier price is greater than the strike" $ do
+        it "the price is calculated" $ do
+          blackscholesprice (BarrierOption (Equity 80) UpAndIn European Put 95 90 1 0.05) 0.10 0.15 `shouldBe` Right 0.32670213171825235
+          
+    context "Given a Up-and-Out European Put Equity Barrier Option" $ do
+      context "When the stock price is less than the barrier" $ do
+        it "it is converted to a vanilla call" $ do
+          blackscholesprice (BarrierOption (Equity 100) UpAndOut European Put 90 100 1 0.05) 0.10 0.15 `shouldBe` Left "Contract is void"
+
+      context "When the barrier price is less than the strike" $ do
+        it "the price is calculated" $ do
+          blackscholesprice (BarrierOption (Equity 80) UpAndOut European Put 90 100 1 0.05) 0.10 0.15 `shouldBe` Right 10.861264375272047
+          
+      context "When the barrier price is greater than the strike" $ do
+        it "the price is calculated" $ do
+          blackscholesprice (BarrierOption (Equity 80) UpAndOut European Put 95 90 1 0.05) 0.10 0.15 `shouldBe` Right 7.52257863686332
+          
