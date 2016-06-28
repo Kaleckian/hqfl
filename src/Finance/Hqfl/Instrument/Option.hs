6f15dcc5c03d27e0524bcc0e0fcc42c0b2326d69
Michael Khan <co.kleisli@gmail.com>
2016-06-28 16:26:51 +0100

    remove dividend yield field

--- src/Finance/Hqfl/Instrument/Option.hs
+++ src/Finance/Hqfl/Instrument/Option.hs
@@ -10,57 +10,11 @@
 ----------------------------------------------------------------------------   
 
 module Finance.Hqfl.Instrument.Option
-(
-  Option(..)
-)
+ (
+   Option (..)
+ )
 where
-
 
 import Finance.Hqfl.Instrument.Type
 
-{-
-
-Classifications:
-
-By Option Rights:
-
-1. Call
-2. Put
-
-By Underlying Asserts:
-
-1. Equity Option
-2. Bond Option
-3. Future Option
-4. Index Option
-5. Commodity Option
-6. Currency Option
-
-By Option Styles
-
-1. American
-2. European
-
-American and European are referred to as Vanilla Options
-
-Exotic options are non-Vanilla options:
-
-1. Bermudan
-2. Asian
-3. Barrier
-4. Binary
-
--}
--- TODO: Refactor into a typeclass?
--- TODO: Function to calculate time to maturity?
--- TODO: Function to get the current price of the underlying
--- TODO: Typeclasses to categorise options according to their pricing function: American, European - each instance to supply pricing method
-
-data Option a = Option a Type Style Strike Maturity Dividend deriving (Show)
-
--- experimenting; review later
---instance Eq a => Eq (Option a) where
---  Option a1 b1 c1 d1 e1 == Option a2 b2 c2 d2 e2 = a1 == a2 && b1 == b2 && c1 == c2 && d1 == d2 && e1 == e2
-    
---instance Functor Option  where
---  fmap f (Option a b c d e) = Option (f a) b c d e
+data Option a = Option a Type Style Strike Expiration deriving (Show)
