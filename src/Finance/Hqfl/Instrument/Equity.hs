f68b0c37bd39b2841e54823465c4c68b5a210d4f
Michael Khan <co.kleisli@gmail.com>
2016-06-29 03:26:14 +0100

    add banner

--- src/Finance/Hqfl/Instrument/Equity.hs
+++ src/Finance/Hqfl/Instrument/Equity.hs
@@ -1,5 +1,16 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Finance.Hqfl.Instrument.Equity
+-- Copyright   :  (C) 2016 Mika'il Khan
+-- License     :  (see the file LICENSE)
+-- Maintainer  :  Mika'il Khan <co.kleisli@gmail.com>
+-- Stability   :  stable
+-- Portability :  portable
+--
+----------------------------------------------------------------------------   
+
 module Finance.Hqfl.Instrument.Equity
-       where
+  where
 
 import Finance.Hqfl.Instrument.Type
 
