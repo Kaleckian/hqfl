56f6fac4a368eed2af8c8d82077677fd32525c6f
Michael Khan <co.kleisli@gmail.com>
2016-06-29 03:25:34 +0100

    add banner

--- src/Finance/Hqfl.hs
+++ src/Finance/Hqfl.hs
@@ -1,5 +1,16 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Finance.Hqfl
+-- Copyright   :  (C) 2016 Mika'il Khan
+-- License     :  (see the file LICENSE)
+-- Maintainer  :  Mika'il Khan <co.kleisli@gmail.com>
+-- Stability   :  stable
+-- Portability :  portable
+--
+----------------------------------------------------------------------------   
+
 module Finance.Hqfl
-       ( module Finance.Hqfl.Instrument
-       ) where
+  ( module Finance.Hqfl.Instrument
+  ) where
 
 import Finance.Hqfl.Instrument
