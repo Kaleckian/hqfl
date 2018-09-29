9c204f47e9c0294006d6b8dd25fec866958cd498
Mika'il Khan <co.category@gmail.com>
2018-09-29 21:14:16 +0100

    add convention imports

---
+++ src/Finance/Hqfl/Convention.hs
@@ -0,0 +1,20 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Finance.Hqfl.Convention
+-- Copyright   :  (C) 2018 Mika'il Khan
+-- License     :  (see the file LICENSE)
+-- Maintainer  :  Mika'il Khan <co.category@gmail.com>
+-- Stability   :  stable
+-- Portability :  portable
+--
+----------------------------------------------------------------------------   
+
+module Finance.Hqfl.Convention
+  (
+    module Finance.Hqfl.Convention.DayCount
+  , module Finance.Hqfl.Convention.BusinessDay
+  )
+  where
+
+import Finance.Hqfl.Convention.DayCount
+import Finance.Hqfl.Convention.BusinessDay
