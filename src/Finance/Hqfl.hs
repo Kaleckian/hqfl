adb2fb8dd63aceadc3cb5844033470d1d7292c19
Mika'il Khan <co.category@gmail.com>
2018-09-29 21:17:32 +0100

    add convention module

--- src/Finance/Hqfl.hs
+++ src/Finance/Hqfl.hs
@@ -1,9 +1,9 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Finance.Hqfl
--- Copyright   :  (C) 2016 Mika'il Khan
+-- Copyright   :  (C) 2018 Mika'il Khan
 -- License     :  (see the file LICENSE)
--- Maintainer  :  Mika'il Khan <co.kleisli@gmail.com>
+-- Maintainer  :  Mika'il Khan <co.category@gmail.com>
 -- Stability   :  stable
 -- Portability :  portable
 --
@@ -11,11 +11,13 @@
 
 module Finance.Hqfl
   (
-    module Finance.Hqfl.Instrument,
-    module Finance.Hqfl.Currency,
-    module Finance.Hqfl.Rate
+    module Finance.Hqfl.Instrument
+  , module Finance.Hqfl.Currency
+  , module Finance.Hqfl.Rate
+  , module Finance.Hqfl.Convention
   ) where
 
 import Finance.Hqfl.Instrument
 import Finance.Hqfl.Currency
 import Finance.Hqfl.Rate
+import Finance.Hqfl.Convention
